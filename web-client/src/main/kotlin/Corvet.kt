d781783501d212528fb1c4318428b3a9f6b2a662
Martin Pešek <martin@orbu.net>
2019-02-19 01:24:04 +0100

    Kotlin JQuery test

--- web-client/src/main/kotlin/Corvet.kt
+++ web-client/src/main/kotlin/Corvet.kt
@@ -1,8 +1,10 @@
+import js.externals.jquery.jQuery
 import org.w3c.dom.HTMLElement
 import org.w3c.dom.clipboard.ClipboardEvent
 import org.w3c.dom.events.Event
 import org.w3c.dom.events.EventListener
 import kotlin.browser.document
+import kotlin.browser.window
 
 fun main() {
     val rte = document.getElementById("rte") as? HTMLElement ?: return
@@ -18,5 +20,16 @@ fun onPaste(event: Event) {
 
         val data = event.clipboardData ?: return
         println("No. of items pasted: ${data.items.length}")
+
+        window.setTimeout({
+            val rteImg = jQuery("#rte").find("img")
+            if (rteImg != undefined && rteImg.length == 1) {
+                println("found pasted img object")
+                val attr = rteImg.attr("src")
+                println("attr: $attr")
+            } else {
+                println("No rte img object was pasted!")
+            }
+        }, 1)
     }
 }
